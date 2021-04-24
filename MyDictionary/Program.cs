4bc2d080b3265e4724eb6d4c47dbaaa2a0e3989d
Berat Ceylan <Beratcey51@gmail.com>
2021-04-24 15:02:44 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string , int>();
+            myDictionary.Add("Computer", 5);
+            myDictionary.Add("Software", 6);
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
